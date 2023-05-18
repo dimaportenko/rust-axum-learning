839fde07314d405e8d8bebc4962747fd10a2ce2f
Dima Portenko <troublesohard@gmail.com>
2023-05-18 23:02:20 +0300

    path param

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,46 @@
 use std::net::SocketAddr;
 
+use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    // 127.0.0.1:8080/hello
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(handler_hello),
-    );
+    let routes = Router::new().merge(hello_routes());
 
     // region:    --- Start Server ---
+    // 127.0.0.1:8080/hello
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on {}", addr);
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes.into_make_service())
         .await
         .unwrap();
     // endregion:    --- Start Server ---
 }
 
-async fn handler_hello() -> impl IntoResponse {
-    println!("handler_hello");
-
-    Html("Hello, <strong>World!</strong>")
+fn hello_routes() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("handler_hello");
+    let name = params.name.as_deref().unwrap_or("World");
+
+    Html(format!("Hello, {}!", name))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("handler_hello2");
+
+    Html(format!("Hello, {}!", name))
+}
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -7,7 +7,8 @@ use anyhow::Result;
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Dima").await?.print().await?;
+    hc.do_get("/hello2/Jake").await?.print().await?;
 
     Ok(())
 }
